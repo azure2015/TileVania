af573847ad87edd3a8aa2a926d9c95a90f90f8d0
Azure Sky <azuremmii@hotmail.com>
2021-11-05 17:33:39 +0000

    Added Persist levels objects

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int pointForCoinPickUp = 100;
+    
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && collision.GetType() == typeof(CapsuleCollider2D))
         {
+            FindObjectOfType<GameSession>().AddToScore(pointForCoinPickUp);
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
